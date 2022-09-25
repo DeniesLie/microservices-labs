cd1dfdbdbac826f029e0e0d83893eda92da51dd1
fundowakin <fundowakin@gmail.com>
2022-09-25 21:00:47 +0300

    added employee service endpoints

---
+++ src/backend/EmployeeService/EmployeeService/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using EmployeeService.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeService.Controllers
+{
+    [Route("api/employees")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet("{id:int}")]
+        public EmployeeGetDto Get(int id)
+        {
+            return new EmployeeGetDto() {Id = id, Lastname = "Daun", Name = "Artem", PhoneNumber = "123456789"};
+        }
+
+        [HttpGet("GetByStorage/{storageId:int}")]
+        public IEnumerable<EmployeeGetDto> GetEmployeesOfStorage(int storageId)
+        {
+            return new List<EmployeeGetDto>()
+            {
+                new() {Id = 12, Lastname = "Daun", Name = "Artem", PhoneNumber = "123456789"}
+            };
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee([FromBody] EmployeePostDto employeePostDto)
+        {
+            return Created("api/employees", employeePostDto);
+        }
+    }
+}
