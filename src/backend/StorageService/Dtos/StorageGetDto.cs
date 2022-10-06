9b75f1478dc27795c4c1d9fc56d08aba1cc2aad6
Viktor Zozuliuk <viktor.zozuliuk@valtech.com>
2022-10-06 21:52:01 +0300

    Updated storage service files

---
+++ src/backend/StorageService/Dtos/StorageGetDto.cs
@@ -0,0 +1,10 @@
+﻿namespace StorageService.Dtos;
+
+public class StorageGetDto
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Address { get; set; }
+}
---
+++ src/backend/StorageService/Dtos/StoragePostDto.cs
@@ -0,0 +1,8 @@
+﻿namespace StorageService.Dtos;
+
+public class StoragePostDto
+{
+    public string? Name { get; set; }
+
+    public string? Address { get; set; }
+}
