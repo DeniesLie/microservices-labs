eb0f759b9b3958b22db7973cddb060a84b6502d1
= <=>
2022-10-14 15:53:48 +0300

    Added client to scripts

--- src/scripts/delete_all_services.ps1
+++ src/scripts/delete_all_services.ps1
@@ -2,3 +2,4 @@ kubectl delete -f $PSScriptRoot/../k8s/storage-service/
 kubectl delete -f $PSScriptRoot/../k8s/transaction-service/
 kubectl delete -f $PSScriptRoot/../k8s/item-service/
 kubectl delete -f $PSScriptRoot/../k8s/employee-service/
+kubectl delete -f $PSScriptRoot/../k8s/client/
--- src/scripts/deploy_all_services.ps1
+++ src/scripts/deploy_all_services.ps1
@@ -2,3 +2,4 @@ kubectl apply -f $PSScriptRoot/../k8s/storage-service/
 kubectl apply -f $PSScriptRoot/../k8s/transaction-service/
 kubectl apply -f $PSScriptRoot/../k8s/item-service/
 kubectl apply -f $PSScriptRoot/../k8s/employee-service/
+kubectl apply -f $PSScriptRoot/../k8s/client/
