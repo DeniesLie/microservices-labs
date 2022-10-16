c7c831fc60a3acf930eab96623b9d3cb1a4808c4
yehor.kalchenko <yehor.kalchenko@valtech.com>
2022-10-16 13:55:42 +0300

    added components

--- src/frontend/client-app/src/app/features/employee/employee.module.ts
+++ src/frontend/client-app/src/app/features/employee/employee.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EmployeeManagerComponent } from './components/employee-manager/employee-manager.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EmployeeManagerComponent
+  ],
   imports: [
     CommonModule
   ]
