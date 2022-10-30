baee05f035295aabfa1840275593ac2473c302d0
Vitaliy <vitalik.vinnik.05@gmail.com>
2022-10-30 19:47:43 +0300

    add db and modify dockerfile

---
+++ src/backend/ItemService/Data/ModelBuilderExtensions.cs
@@ -0,0 +1,34 @@
+﻿using ItemService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ItemService.Data;
+
+public static class ModelBuilderExtensions
+{
+    public static void SeedData(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Item>().HasData(SeedItems());
+    }
+    
+    private static List<Item> SeedItems()
+    {
+        return new List<Item>()
+        {
+            new Item()
+            {
+                Id = Guid.Parse("03dec121-ac0e-4189-bd9e-68013cd175f9"),
+                Name = "Javelin"
+            },
+            new Item()
+            {
+                Id = Guid.Parse("7c164a1e-84ab-44b9-9542-9c0a681172a7"),
+                Name = "NLAW"
+            },
+            new Item()
+            {
+                Id = Guid.Parse("8f515945-5f20-4acb-95fc-b0aa2eb91497"),
+                Name = "Switchblade 300"
+            }
+        };
+    }
+}
---
+++ src/backend/ItemService/Services/Abstract/IItemService.cs
@@ -0,0 +1,12 @@
+﻿using ItemService.DTOs;
+
+namespace ItemService.Services.Abstract;
+
+public interface IItemService
+{
+    Task<ItemGetDto> CreateAsync(ItemPostDto itemPostDto);
+    Task<List<ItemGetDto>> GetAllAsync();
+    Task<ItemGetDto> GetByIdAsync(Guid id);
+    Task<ItemGetDto> UpdateAsync(ItemPutDto itemPutDto);
+    Task DeleteAsync(Guid id);
+}
