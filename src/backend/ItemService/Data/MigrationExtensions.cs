b7911040d08800910e8f775e1a827afa7d2cb279
Vitaliy <vitalik.vinnik.05@gmail.com>
2022-10-31 00:57:02 +0300

    add auto applying of migration

---
+++ src/backend/ItemService/Data/MigrationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ItemService.Data;
+
+public static class MigrationsExtension
+{
+    public static void ApplyMigrations(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<ItemContext>();
+        
+        if (dbContext is not null && app.Environment.IsProduction())
+        {
+            try
+            {
+                dbContext.Database.Migrate();
+            }
+            catch (Exception ex)
+            {
+                app.Logger.LogError($"Could not apply migrations. {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
