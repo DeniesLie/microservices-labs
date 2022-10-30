b15f9479050bb277f0e53e81fc88ae9a65754fee
= <=>
2022-10-30 22:30:10 +0200

    Added crud operations

--- src/frontend/client-app/src/app/features/transaction/services/transaction-service/transaction.service.ts
+++ src/frontend/client-app/src/app/features/transaction/services/transaction-service/transaction.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, throwError } from 'rxjs';
-import { Transaction } from 'src/app/core/entities/transaction';
+import { catchError, Observable, pipe, throwError } from 'rxjs';
+import { Transaction, TransactionCreate, TransactionUpdate } from 'src/app/core/entities/transaction';
 import { environment } from 'src/environments/environment';
 
 
@@ -15,16 +15,33 @@ export class TransactionService {
       'Content-Type': 'application/json',
     }),
   };
+  baseUrl = `${environment.apiUrl}/transactionservice/api/transactions`;
 
   constructor(
     private _httpClient: HttpClient
   ) { }
 
-  getAll(): Observable<Transaction[]> {
-    const url = `${environment.apiUrl}/transactionservice/api/transactions`; 
+  getByStorageId(storageId: string): Observable<Transaction[]> {
+    const url = `${this.baseUrl}/${storageId}`; 
     return this._httpClient.get<Transaction[]>(url).pipe(
-      catchError(err => this.handleError('get messages', err))
+      catchError(err => this.handleError('get transactions by storageId', err))
     )
+  }
+
+  create(transaction: TransactionCreate): Observable<Transaction> {
+    return this._httpClient.post<Transaction>(this.baseUrl, transaction, this.httpOptions)
+      .pipe(catchError(err => this.handleError('create transaction', err)))
+  }
+
+  updateNotes(transaction: TransactionUpdate): Observable<Transaction> {
+    return this._httpClient.put<Transaction>(this.baseUrl, transaction, this.httpOptions)
+      .pipe(catchError(err => this.handleError('update transaction', err)))
+  }
+
+  deleteNotes(transactionId: string) {
+    const url = `${this.baseUrl}/${transactionId}`;
+    return this._httpClient.delete(url)
+      .pipe(catchError(err => this.handleError('delete transaction', err)))
   }
 
   private handleError(method: string, err: any) {
