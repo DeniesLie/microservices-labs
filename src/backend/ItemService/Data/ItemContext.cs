ac2ac5cd3d1ce8fcd2424c0aaa388552e3ba2b21
Vitaliy <vitalik.vinnik.05@gmail.com>
2022-11-17 17:53:30 +0200

    remove applying migration

--- src/backend/ItemService/Data/ItemContext.cs
+++ src/backend/ItemService/Data/ItemContext.cs
@@ -7,7 +7,10 @@ public class ItemContext: DbContext
 {
     public DbSet<Item> Items { get; set; }
 
-    public ItemContext(DbContextOptions<ItemContext> options): base(options) { }
+    public ItemContext(DbContextOptions<ItemContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
