7dd72ea2981d0bf5813a4f4fca2c1c61dde5b6b6
DeniesLie <lienko.d.k@gmail.com>
2022-11-20 17:34:13 +0200

    Added loading of storages and items from backend

--- src/frontend/client-app/src/app/features/transaction/components/create-dialog/create-dialog.component.ts
+++ src/frontend/client-app/src/app/features/transaction/components/create-dialog/create-dialog.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Transaction, TransactionCreate } from 'src/app/core/entities/transaction';
 import { TransactionType } from 'src/app/core/enums/TransactionType';
 import { TransactionService } from '../../services/transaction-service/transaction.service';
 import { FormBuilder, FormControlStatus } from '@angular/forms'; 
+import { Item } from 'src/app/core/entities/item';
+import { StorageModel } from 'src/app/core/entities/storage';
+import { ItemService } from 'src/app/features/item/services/item.service';
+import { StorageService } from 'src/app/features/storage/services/storage-service/storage.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-create-dialog',
   templateUrl: './create-dialog.component.html',
   styleUrls: ['./create-dialog.component.css']
 })
-export class CreateDialogComponent implements OnInit {
+export class CreateDialogComponent implements OnInit, OnDestroy {
+
+  public items: Item[] = [];
+  public storages: StorageModel[] = [];
+
+  private unsubscribeAll: Subject<any>;
 
   dataForm: FormGroup = this.fb.group({
     itemId: ['', Validators.required],
@@ -24,10 +34,19 @@ export class CreateDialogComponent implements OnInit {
   constructor(
     private transactionService: TransactionService,
     private dialogRef: MatDialogRef<CreateDialogComponent>,
-    private fb: FormBuilder
-  ) { }
+    private fb: FormBuilder,
+    private itemService: ItemService,
+    private storageService: StorageService
+  ) { 
+    this.unsubscribeAll = new Subject();
+  }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribeAll.next(true);
   }
 
   create(){
@@ -37,4 +56,30 @@ export class CreateDialogComponent implements OnInit {
       this.dialogRef.close()
     })
   }
+
+  private initForm() {
+    this.initItems();
+    this.initStorages();
+  }
+
+  private initItems() {
+    this.itemService.getAll()
+      .pipe(takeUntil(this.unsubscribeAll))
+      .subscribe(items => {
+        if (items) {
+          this.items = items;
+        }
+      })
+  }
+
+  private initStorages() {
+    this.storageService.getAll()
+      .pipe(takeUntil(this.unsubscribeAll))
+      .subscribe(storages => {
+        if (storages) {
+          this.storages = storages;
+        }
+      })
+  } 
+
 }
